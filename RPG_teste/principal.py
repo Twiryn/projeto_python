7cf559f7afbc6be372f6501c9ed2c23ff95d22dc
Miguel_Matias <129789158+Twiryn@users.noreply.github.com>
2025-06-19 14:12:11 -0300

    Update principal.py

--- RPG_teste/principal.py
+++ RPG_teste/principal.py
@@ -1,6 +1,6 @@
 from funcionalidades import *
 from tela import *
 titulos('RPG')
-NomeJogador=input('NICKNAME:')
+NomeJogador=input('NOME:')
 titulos(f'BEM VINDO {NomeJogador.upper()}')
 personagem.classes()
