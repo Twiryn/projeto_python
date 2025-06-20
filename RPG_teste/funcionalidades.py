a85a39685435f0e9936bcca37ace9cd8fd2fb09e
Miguel_Matias <129789158+Twiryn@users.noreply.github.com>
2025-06-20 20:09:05 -0300

    Update funcionalidades.py

--- RPG_teste/funcionalidades.py
+++ RPG_teste/funcionalidades.py
@@ -13,10 +13,10 @@ class personagem:
               f'\nAP:  {personagem.ap}'
               f'\nATK: {personagem.atk}')
         #CLASSES EXISTENTES
-def classes():
+    def classes():
          guerreiro=personagem('GUERREIRO',900,300,35)
          mago=personagem('MAGO',600,800,40)
-         assasino=personagem('ASSASSINO',780,530,45)
+         assassino=personagem('ASSASSINO',780,530,45)
          tank=personagem('TANK', 1200, 380, 30)
         #MOSTRA OS STATUS DE CADA CLASSE
          while True:    
@@ -25,29 +25,45 @@ def classes():
             Listaclasses()
             titulos('\033[33mSELEÇÃO DE CLASSE PARA O JOGADOR\033[m')
             try:
-                VerStatus=int(input('DIGITE O NUMERO DA CLASSE PARA VER OS STATUS:')) 
-                if VerStatus == 1:
+                VerStatus=input('DIGITE O NOME DA CLASSE PARA VER OS STATUS:').strip().upper()
+                if VerStatus == 'GUERREIRO':
                     titulos('\033[33m GUERREIRO\033[m')
-                    personagem.ExibirStatus()
-                elif VerStatus == 2:
+                    personagem.ExibirStatus(guerreiro)
+                elif VerStatus == 'MAGO':
                     titulos('\033[33m MAGO\033[m')
                     personagem.ExibirStatus(mago)
-                elif VerStatus == 3:
+                elif VerStatus == 'ASSASSINO':
                     titulos('\033[33m ASSASSINO\033[m')
-                    personagem.ExibirStatus(assasino)
-                elif VerStatus == 4:
+                    personagem.ExibirStatus(assassino)
+                elif VerStatus == 'TANK':
                     titulos('\033[33m TANK\033[m')
                     personagem.ExibirStatus(tank)
+                elif VerStatus == 'SAIR DO JOGO':
+                    print('\033[33m SAINDO DO JOGO... \033[m')
+                    sleep(1)
+                    break
                 else:
                  print('\033[31mESTA CLASSE NAO EXISTE, TENTE NOVAMENTE\033[m')
-                 linhas()
                  continue
+                 linhas()
+            except(ValueError,TypeError):
+               print('\033[31mVALOR INCORRETO, TENTE NOVAMENTE\033[m ')
+            except KeyboardInterrupt:
+               print('\n\033[31mPROGRAMA INTERROMPIDO PELO USUARIO\033[m')
+               break
                 #CONFIRMA A SELEÇÃO DE CLASSE
-                while True:
-                 SelecionarPersonagem=input('VOCE IRA SELECIONAR ESTE PERSONAGEM?[S/N]').upper() 
-                 if SelecionarPersonagem == 'N':
+            try:
+                SelecionarPersonagem=input('VOCE IRA SELECIONAR ESTE PERSONAGEM?[S/N]').upper() 
+                if 'S' != SelecionarPersonagem != 'N':
+                    print('\033[31mERRO, SOMENTE [S/N]\033[m')
+                elif SelecionarPersonagem == 'S':
+                    print(f'\033[33mSELECIONOU A CLASSE {VerStatus}\033[m')
+                    print('O JOGO IRA COMEÇAR...')
+                    sleep(1.5)
+                    break
+                elif SelecionarPersonagem == 'N':
                   linhas()
-                  break
+                  continue
             except(ValueError,TypeError):
                print('\033[31mVALOR INCORRETO, TENTE NOVAMENTE\033[m ')
             except KeyboardInterrupt:
