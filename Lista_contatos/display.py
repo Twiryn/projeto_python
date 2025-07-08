6e0edc8d3838be9e9009c8d535779223784c3eb5
Miguel_Matias <129789158+Twiryn@users.noreply.github.com>
2025-07-08 13:18:32 -0300

    Parte visual

---
+++ Lista_contatos/display.py
@@ -0,0 +1,11 @@
+def linha(tam=42):
+    print('-' * tam)
+def titulos(msg):
+    print('-'*30)
+    print(msg.center(30))
+    print('-'*30)
+def opçoes():
+    print('1-Adicionar Contatos',
+          '\n2-Listar Contatos',
+          '\n3-Remover Contatos',
+          '\n4-Sair do programa')
