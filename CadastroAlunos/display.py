772e5d18ac6d47723384f2f2657d4d5aa7543690
Miguel_Matias <129789158+Twiryn@users.noreply.github.com>
2025-08-12 09:56:00 -0300

    Update display.py

--- CadastroAlunos/display.py
+++ CadastroAlunos/display.py
@@ -5,6 +5,7 @@ def menu(txt):
 def opçoes():
     print('1-ADICIONAR ALUNO',
           '\n2-LISTAR ALUNO',
-          '\n3-REMOVER ALUNO',
-          '\n4-ATUALIZAR ALUNO',
+          '\n3-ATUALIZAR ALUNO',
+          '\n4- REMOVER ALUNO',
+
           '\n5-SAIR DO PROGRAMA')
